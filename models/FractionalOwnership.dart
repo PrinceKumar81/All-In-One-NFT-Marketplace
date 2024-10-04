f3df6083b44d4dafcfcc875e3887e5c23685858b
Prince Kumar <princekr81022@gmail.com>
2024-10-04 09:55:52 +0530

    Add files via upload

---
+++ models/FractionalOwnership.dart
@@ -0,0 +1,14 @@
+class FractionalOwnerShip {
+  final String name;
+  final String imageUrl;
+  final int favoriteNumber;
+  final int CommentNumber;
+  final String ownership;
+  FractionalOwnerShip({
+    required this.name,
+    required this.imageUrl,
+    required this.favoriteNumber,
+    required this.CommentNumber,
+    required this.ownership,
+  });
+}
---
+++ models/nft_card_model.dart
@@ -0,0 +1,12 @@
+class NFTCardModel {
+  final String name;
+  final String imageUrl;
+  final int favoriteNumber;
+  final int CommentNumber;
+  NFTCardModel({
+    required this.name,
+    required this.imageUrl,
+    required this.favoriteNumber,
+    required this.CommentNumber,
+  });
+}
